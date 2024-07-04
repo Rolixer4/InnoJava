429b10e60b851a1e99ed83b54fa821b5f4356442
Rolixer4 <shevaldin.nikita@mail.ru>
2024-07-05 01:06:20 +0500

    Task1

--- IntermediateCertification/Task1/Task1.java
+++ IntermediateCertification/Task1/Task1.java
@@ -1,5 +1,6 @@
 package ru.inno.IntermediateCertification.Task1;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
@@ -10,20 +11,30 @@ public class Task1 {
 
         System.out.println("Введите длину забора");
         Scanner scanner = new Scanner(System.in);
-        int fenceLength = scanner.nextInt();
+        try {
+            int fenceLength = scanner.nextInt();
+            int countsOfChars = 0;
+            int countsOfSpaces = 0;
 
-        int countsOfChars = 0;
-        int countsOfSpaces = 0;
+            String declarationOfLove = "признание в любви ";
 
-        String declarationOfLove = "признание в любви";
-
-        for (char c : declarationOfLove.toCharArray()) {
-            if (c == ' ') {
-                countsOfSpaces += 1;
-            } else {
-                countsOfChars += 1;
+            for (char c : declarationOfLove.toCharArray()) {
+                if (c == ' ') {
+                    countsOfSpaces += 1;
+                } else {
+                    countsOfChars += 1;
+                }
             }
+
+            double math = (countsOfChars / 3) * 62 + countsOfSpaces * 12;
+
+            if (math < fenceLength) {
+                System.out.println("Шалость удалась!");
+            } else {
+                System.out.println("Все пропало! Длина забора " + fenceLength + ", а длина признания " + math);
+            }
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println("Ошибка! введите пожалуйста целочисленную длину забора");
         }
-        System.out.println(countsOfChars);
     }
 }
